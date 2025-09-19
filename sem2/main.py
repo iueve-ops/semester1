a31204fde3ed1a2e182c0f4395c095a960a5a6d5
Eve <iueve@yandex.ru>
2025-09-19 14:24:10 +0300

    dz2

--- sem2/main.py
+++ sem2/main.py
@@ -1,3 +1,16 @@
-a = 1+1
+p = list(map(str, input()))
+z1 = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y', '1', '8', 'E', 'J', 'S', 'Z', '3', 'L', '2', '5']
+a = 0
+l = 0
+#for i in range(len(p)):
+    for l in p:
+        if p[l] in z1:
+            a += 1
+    if a == len(p):
+        print("буквы да")
+
+
+
+
 print(a)
 
